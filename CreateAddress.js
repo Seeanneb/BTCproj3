b600f17e565e52e9a7b0e9a54b24883c678a5555
Seeanneb <Seeanneb@gmail.com>
2018-01-17 20:21:35 -0800

    segwit addresses and mongodb added

--- CreateAddress.js
+++ CreateAddress.js
@@ -1,14 +1,24 @@
 var bitcoin = require('bitcoinjs-lib');
 var fs = require('fs');
+var crypto = require('crypto')
 
 var outputFile = "wallet.dat"
 
 // var stream = fs.createWriteStream(outputFile);
 
-var keyPair = bitcoin.ECPair.makeRandom({network: bitcoin.networks.testnet});
-var private = keyPair.toWIF();
-var address = keyPair.getAddress();
-fs.appendFile(outputFile, "Private key: " + private + "\n" + "Address: " + address + "\n")
-console.log("Keys Created! they are located in your wallet.dat file")
+// var keyPair = bitcoin.ECPair.makeRandom({network: bitcoin.networks.testnet});
+// var private = keyPair.toWIF();
+// var address = keyPair.getAddress();
+// fs.appendFile(outputFile, "Private key: " + private + "\n" + "Address: " + address + "\n")
+// console.log("Keys Created! they are located in your wallet.dat file")
 
+// Segwit Address
+var keyPair = bitcoin.ECPair.makeRandom({network: bitcoin.networks.testnet});
+var pubKey = keyPair.getPublicKeyBuffer()
+
+var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
+var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+var address = bitcoin.address.fromOutputScript(scriptPubKey)
+
+console.log(address)
 
