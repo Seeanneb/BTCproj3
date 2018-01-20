3badb0d11133bf48bb38beb9c84ee91a98347bde
PASS <seeanneb@gmail.com>
2018-01-19 16:44:11 -0800

    Mnemnic address generating

--- createMnemonic.js
+++ createMnemonic.js
@@ -10,14 +10,40 @@ var entropy = crypto.randomBytes(16)
 
 var mnemonic = bip39.entropyToMnemonic(entropy.toString('hex'))
 var seed = bip39.mnemonicToSeed(mnemonic)
+var seedHex = bip39.mnemonicToSeedHex(mnemonic)
 
-var mnemonicArray = mnemonic.split(" ")
-// console.log(mnemonicArray)
+var hdMaster = bitcoin.HDNode.fromSeedHex(seedHex)
 
-
-
-fs.appendFile(outputFile, "Mnemonic Seed: " + mnemonic + "\n" + "Seed Hex: " + hexSeed + "\n")
-// console.log(Array.isArray(mnemonic)) FALSE
+var keyChain = []
 
 
+function generateKeyChain(){
+for (var i = 0; i < 20; i++){
+	var key = hdMaster.derivePath('m/' + [i])
+	keyChain.push(key)
+ }
+}
+
+function generateAddress(){
+for (var j = 0; j < keyChain.length; j++){
+	var pubKey = keyChain[j].keyPair.getPublicKeyBuffer()
+	var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
+    var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+    var address = bitcoin.address.fromOutputScript(scriptPubKey)
+    console.log(address)
+ }
+}
+generateKeyChain();
+generateAddress();
+
+// var key0 = hdMaster.derivePath('m/0')
+// var key1 = hdMaster.derivePath('m/1')
+
+// var pubKey = key.keyPair.getPublicKeyBuffer()
+// var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
+// var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+// var address = bitcoin.address.fromOutputScript(scriptPubKey)
+
+// console.log(mnemonic)
+// console.log(keyChain)
 
