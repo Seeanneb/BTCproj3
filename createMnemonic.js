771124055637ae909dfdd4ee4e7b1e21ed19b441
Sean Byrne <seeanneb@gmail.com>
2018-01-23 10:08:11 -0800

    continue on auth w/ wallet creation additions

--- createMnemonic.js
+++ createMnemonic.js
@@ -1,49 +1,64 @@
 var bitcoin = require('bitcoinjs-lib');
 var bip39 = require('bip39');
 var fs = require('fs');
-var prompt = require('cli-prompt')
+// var prompt = require('cli-prompt')
 var crypto = require('crypto')
 
 var outputFile = "wallet.dat"
-
 var entropy = crypto.randomBytes(16)
-
-var mnemonic = bip39.entropyToMnemonic(entropy.toString('hex'))
-var seed = bip39.mnemonicToSeed(mnemonic)
-var seedHex = bip39.mnemonicToSeedHex(mnemonic)
-
-var hdMaster = bitcoin.HDNode.fromSeedHex(seedHex)
-
 var keyChain = []
+var mnemonic = bip39.entropyToMnemonic(entropy.toString('hex'))
 
 
-function generateKeyChain(){
-for (var i = 0; i < 20; i++){
-	var key = hdMaster.derivePath('m/' + [i])
-	keyChain.push(key)
- }
+function createWallet(){
+	// var mnemonic = bip39.entropyToMnemonic(entropy.toString('hex'))
+	var seed = bip39.mnemonicToSeed(mnemonic)
+	var seedHex = bip39.mnemonicToSeedHex(mnemonic)
+	var hdMaster = bitcoin.HDNode.fromSeedHex(seedHex) 
+	console.log(mnemonic)
+	// return mnemonic;
+
+	for (var i = 0; i < 20; i++) {
+		var key = hdMaster.derivePath('m/' + [i])
+		keyChain.push(key)
+	}
+	for (var j = 0; j < keyChain.length; j++){
+		var pubKey = keyChain[j].keyPair.getPublicKeyBuffer()
+		var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
+	    var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+	    var address = bitcoin.address.fromOutputScript(scriptPubKey)
+
+	    console.log((j+1) + " " +address)
+
+	 }
+	 
 }
 
-function generateAddress(){
-for (var j = 0; j < keyChain.length; j++){
-	var pubKey = keyChain[j].keyPair.getPublicKeyBuffer()
-	var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
-    var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
-    var address = bitcoin.address.fromOutputScript(scriptPubKey)
-    console.log(address)
- }
+function addKeys(){
+	var seed = bip39.mnemonicToSeed(mnemonic)
+	var seedHex = bip39.mnemonicToSeedHex(mnemonic)
+	var hdMaster = bitcoin.HDNode.fromSeedHex(seedHex) 
+	console.log(mnemonic)
+
+	var newLenght = keyChain.length + 5;
+
+	for (var i = keyChain.length; i < newLenght  ; i++) {
+		var key = hdMaster.derivePath('m/' + [i] )
+		keyChain.push(key)
+	}
+	for (var j = 0; j < keyChain.length; j++){
+		var pubKey = keyChain[j].keyPair.getPublicKeyBuffer()
+		var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
+	    var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+	    var address = bitcoin.address.fromOutputScript(scriptPubKey)
+
+	    console.log((i+1) + " " +address)
+	 }
 }
-generateKeyChain();
-generateAddress();
 
-// var key0 = hdMaster.derivePath('m/0')
-// var key1 = hdMaster.derivePath('m/1')
 
-// var pubKey = key.keyPair.getPublicKeyBuffer()
-// var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey))
-// var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
-// var address = bitcoin.address.fromOutputScript(scriptPubKey)
+createWallet();
+// console.log(mnemonicArray)
+addKeys();
 
-// console.log(mnemonic)
-// console.log(keyChain)
 
